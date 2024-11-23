4507faf561e4bd0217de63d549d60086ae81de70
nikhildixit-321 <nikhildixit525@gamil.com>
2024-11-23 10:06:39 +0530

    fifth commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 
     const navigate = useNavigate();
     const [showMenu , setShowMenu]= useState(false)
-    const [token, setToken]= useState(false )
+    const [token, setToken]= useState(true )
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
       <img onClick={()=>{navigate('/')}} className='w-44 cursor-pointer ' src={assets.logo} alt="" />
@@ -16,7 +16,7 @@ const Navbar = () => {
             <li  className='py-1 '>Home</li>
             <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
         </NavLink >
-        <NavLink to='/doctor'>
+        <NavLink to='/doctors'>
             <li className='py-1 '>All Doctor</li>
             <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto  hidden'/>
         </NavLink>
@@ -34,12 +34,12 @@ const Navbar = () => {
             token ? <div className='flex items-center gap-2 cursor-pointer group-relative'> 
                 <img className='w-8 rounded-full' src={assets.profile_pic} alt="" />
                 <img className='w-2.5' src={assets.dropdown_icon} alt="" />
-                <div className='absolute top-12 right-0 text-base font-medium text-gray-600 hidden group-hover:block'>
+                <div className='absolute top-12 right-0 text-base font-medium text-gray-600 hidden hover:block '>
 
                     <div className='min-w-848 bg-stone rounded flex flex-col gap-4 p-4'>
-                        <p onClick={()=>{navigate('my-profile')}} className='horver:text-black cursor-pointer'>My Profile</p>
+                        <p onClick={()=>{navigate('my-profile')}} className='hover:text-black cursor-pointer'>My Profile</p>
                         <p onClick={()=>{navigate('/my-appointment')}} className='hover:text-black cursor-pointer'> My Appointment</p>
-                        <p onClick={()=> navigate(false)} className='horver:text-black cursor-pointer'>Logout</p>
+                        <p onClick={()=> navigate(false)} className='hover:text-black cursor-pointer'>Logout</p>
                     </div>
                 </div>
 
--- frontend/src/pages/Doctor.jsx
+++ frontend/src/pages/Doctor.jsx
@@ -20,17 +20,18 @@ const Doctor = () => {
    applyFilter()
  },[doctors,speciality])
   return (
-    <div className='flex flec-col '>
-      <p className='text-center '>Browser through the doctors specialities.</p>
-      <div>
-        <div>
-          <p>general physician </p>
-          <p>gynecologist</p>
-          <p> dermatologist</p>
-          <p>pediatrician</p>
-          <p>neurologist</p>
-          <p>gastroenterologist</p>
+    <div >
+      <p className='text-gray-600 '>Browser through the doctors specialities.</p>
+      <div className='flex flex-col sm:flex-row items-start gap-5 mt-5 '>
+        <div className=' flex flex-col gap-4 text-sm text-gray-600 '>
+          <p onClick={()=>{speciality=='Gynecologist'? navigate('/doctors'):navigate('/doctors/Gynecologist')}} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded trasition-all cursor-pointer ${speciality==="Gynecologist"?"bg-indigo-100 text-black":""}`}>Gynecologist</p>
+          <p onClick={()=>{speciality=='General physician '? navigate('/doctors'):navigate('/doctors/General physician')}} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded trasition-all cursor-pointer ${speciality==="General physician "?"bg-indigo-100 text-black":""}`}>General physician </p>
+          <p onClick={()=>{speciality==' Dermatologist'? navigate('/doctors'):navigate('/doctors/Dermatologist')}} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded trasition-all cursor-pointer ${speciality==="Dermatologist"?"bg-indigo-100 text-black":""}`}>Dermatologist</p>
+          <p onClick={()=>{speciality==' Pediatricians'? navigate('/doctors'):navigate('/doctors/Pediatricians')}} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded trasition-all cursor-pointer ${speciality==="Pediatricians"?"bg-indigo-100 text-black":""}`}>Pediatricians</p>
+          <p onClick={()=>{speciality=='Neurologist'? navigate('/doctors'):navigate('/doctors/Neurologist')}} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded trasition-all cursor-pointer ${speciality==="Neurologist"?"bg-indigo-100 text-black":""}`}>Neurologist</p>
+          <p onClick={()=>{speciality=='Gastroenterologist'? navigate('/doctors'):navigate('/doctors/Gastroenterologist')}} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded trasition-all cursor-pointer ${speciality==="Gastroenterologist"?"bg-indigo-100 text-black":""}`}>Gastroenterologist</p>
         </div>
+        <div className='w-full grid grid-cols-auto gap-4 gap-y-6'>
         {
           filterDoc.map((item,index)=>(
             <div onClick={()=>{navigate(`/appointment/${item._id}`)}} className='border-blue-200 round-xl overflow-hidden cursor-pointer hover:translate-y-[-1opx] transition-all duration-500' key={index}>
@@ -48,6 +49,7 @@ const Doctor = () => {
         )
 
         )}
+        </div>
         
       </div>
     </div>
