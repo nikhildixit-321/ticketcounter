821ad2fba798a938461c3fdb6651c9f34cc7aeff
nikhildixit-321 <nikhildixit525@gamil.com>
2024-11-24 18:55:58 +0530

    ninth commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { assets } from '../assets/assets_frontend/assets'
+// import { assets } from '../assets/assets_frontend/assets'
+import {assets} from '../assets/assets_frontend/assets'
 
 const Footer = () => {
   return (
